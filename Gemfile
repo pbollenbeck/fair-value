5f7fd07a7368a8dccc1e691f372fcde56589b9eb
Peter Bollenbeck <peter.bollenbeck@invision.de>
2023-05-04 08:00:16 +0200

    Run simulations in parallel

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'distribution'
+gem 'parallel'
 gem 'prime'
+gem 'ruby-progressbar'
--- fair-value.rb
+++ fair-value.rb
@@ -1,5 +1,8 @@
+require 'benchmark'
 require 'date'
 require 'distribution'
+require 'parallel'
+require 'ruby-progressbar'
 
 LAST_CLOSING_PRICE = 9.1
 FIRST_SIM_DATE= Date.parse('2023-03-30')
@@ -7,7 +10,8 @@ LAST_SIM_DATE= Date.parse('2027-03-29')
 SIGMA = 0.022114372
 DRIFT = -0.0007733862929
 GRANT_ID = '2023-q1'
-OUTPUT_FILE_NAME = "output-" + GRANT_ID + ".txt"
+OUTPUT_FILE_NAME = "output-#{GRANT_ID}.txt"
+NUMBER_OF_SIMULATIONS = 5_000_000
 
 def trading_day?(date)
   ![0,6].include?(date.wday)
@@ -27,25 +31,23 @@ def forecast(start_price, sigma, number_of_days, drift)
   last_price
 end
 
-days = trading_days(FIRST_SIM_DATE..LAST_SIM_DATE)
-forecasts = []
-j = 0
-average = 0
-length = days.length
-File.open(OUTPUT_FILE_NAME, 'w') do |f|
-  5_000_000.times do |i|
-    last_price = forecast(LAST_CLOSING_PRICE, SIGMA, length, DRIFT)
-    forecasts.push(last_price)
-    average = (average * i + last_price) / (i+1)
-
-    if (i+1) % 10_000 == 0
-      j += 1
-      s = j.to_s + " x 10k average: " + average.round(2).to_s + "\n"
-      print s
-      f.write s
-      f.flush
-    end
+def simulate
+  length = trading_days(FIRST_SIM_DATE..LAST_SIM_DATE).length
+  Parallel.map(1..NUMBER_OF_SIMULATIONS, progress: "Running simulations") do
+    forecast(LAST_CLOSING_PRICE, SIGMA, length, DRIFT)
   end
 end
 
-puts "\nTotal average price: " + (forecasts.sum / forecasts.length).round(2)
+forecasts = simulate
+
+File.open(OUTPUT_FILE_NAME, 'w') do |f|
+  j = 0
+  average = 0.0
+  forecasts.each_with_index do |forecast, i|
+    average = (average * i + forecast) / (i+1)
+    if (i+1) % 10_000 == 0
+      j += 1
+      f.write "#{j} x 10k average: #{average.round(2)}\n"
+    end
+  end
+end
