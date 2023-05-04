a654a74b0b45fd9989598a84738e517c43d98bce
Peter Bollenbeck <peter.bollenbeck@invision.de>
2023-05-04 09:34:47 +0200

    Encapsulate simulation

--- fair-value.rb
+++ fair-value.rb
@@ -5,40 +5,54 @@ require 'parallel'
 require 'ruby-progressbar'
 
 LAST_CLOSING_PRICE = 9.1
-FIRST_SIM_DATE= Date.parse('2023-03-30')
-LAST_SIM_DATE= Date.parse('2027-03-29')
+LAST_TRADING_DAY = Date.parse('2023-03-29')
 SIGMA = 0.022114372
 DRIFT = -0.0007733862929
 GRANT_ID = '2023-q1'
 OUTPUT_FILE_NAME = "output-#{GRANT_ID}.txt"
-NUMBER_OF_SIMULATIONS = 5_000_000
 
-def trading_day?(date)
-  ![0,6].include?(date.wday)
-end
+class MonteCarloSimulation
+  NUMBER_OF_SIMULATIONS = 5_000_000
+  TIME_IN_YEARS = 4
 
-def trading_days(date_range)
-  date_range.select { |date| trading_day?(date) }
-end
-
-def forecast(start_price, sigma, number_of_days, drift)
-  last_price = start_price
-  number_of_days.times do
-    last_price = (last_price * Math.exp(
-      drift + sigma * Distribution::Normal.p_value(rand())
-    )).round(3)
+  def initialize(last_closing_price, last_trading_day, sigma, drift)
+    @last_closing_price = last_closing_price
+    @sigma = sigma
+    @drift = drift
+    first_sim_date = last_trading_day.next_day
+    last_sim_date = last_trading_day >> (12 * TIME_IN_YEARS) # Advance by months
+    @number_of_days = trading_days(first_sim_date..last_sim_date).length
   end
-  last_price
-end
 
-def simulate
-  length = trading_days(FIRST_SIM_DATE..LAST_SIM_DATE).length
-  Parallel.map(1..NUMBER_OF_SIMULATIONS, progress: "Running simulations") do
-    forecast(LAST_CLOSING_PRICE, SIGMA, length, DRIFT)
+  def call
+    Parallel.map(1..NUMBER_OF_SIMULATIONS, progress: "Running simulations") do
+      forecast
+    end
+  end
+
+  private
+
+  def trading_day?(date)
+    ![0,6].include?(date.wday)
+  end
+
+  def trading_days(date_range)
+    date_range.select { |date| trading_day?(date) }
+  end
+
+  def forecast
+    last_price = @last_closing_price
+    @number_of_days.times do
+      last_price = (last_price * Math.exp(
+        @drift + @sigma * Distribution::Normal.p_value(rand())
+      )).round(3)
+    end
+    last_price
   end
 end
 
-forecasts = simulate
+forecasts = MonteCarloSimulation.new(LAST_CLOSING_PRICE, LAST_TRADING_DAY,
+                                     SIGMA, DRIFT).call
 
 File.open(OUTPUT_FILE_NAME, 'w') do |f|
   j = 0
